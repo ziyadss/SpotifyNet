245e4f4dcf1b9300f09edff1b603175ab57bbe10
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-04-03 16:15:09 +0200

    Add Playground project

---
+++ SpotifyNet.Playground/Program.cs
@@ -0,0 +1,47 @@
+﻿using SpotifyNet.Auth;
+using SpotifyNet.Datastructures.Spotify.Authorization;
+using SpotifyNet.WebAPI;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyNet.Playground;
+
+internal class Program
+{
+    private const string AppClientId = "";
+    private const string AppRedirectUri = "http://localhost:3000";
+
+    static async Task Main()
+    {
+        var newToken = false;
+        var scopes = new[] { AuthorizationScope.PlaylistReadPrivate, AuthorizationScope.UserFollowRead };
+
+        var authorizationClient = new AuthorizationClient(AppClientId, AppRedirectUri);
+        var authorizationRepository = new AuthorizationRepository();
+        var authorizationService = new AuthorizationService(authorizationClient, authorizationRepository);
+        var tokenAcquirer = new TokenAcquirer(AppRedirectUri, authorizationService);
+
+        string token;
+        if (newToken)
+        {
+            token = await tokenAcquirer.GetToken(scopes, cancellationToken: default);
+        }
+        else
+        {
+            token = await tokenAcquirer.GetExistingToken(cancellationToken: default);
+        }
+
+        var webAPIClient = new WebAPIClient();
+        var webAPIRepository = new WebAPIRepository(webAPIClient);
+
+        var playlists = await webAPIRepository.GetCurrentUserPlaylists(token, ownerId: "ziyad.ss");
+
+        Console.WriteLine(playlists[0].Name);
+        Console.WriteLine(playlists[0].Tracks!.Total);
+
+        var tracks = await webAPIRepository.GetPlaylistItems(token, playlists[0].Id!);
+        Console.WriteLine(tracks.Count);
+        Console.WriteLine(string.Join(',', tracks.Select(t => t.Track!.Name)));
+    }
+}
---
+++ SpotifyNet.Playground/TokenAcquirer.cs
@@ -0,0 +1,58 @@
+﻿using SpotifyNet.Auth.Interfaces;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpotifyNet.Playground;
+
+internal class TokenAcquirer
+{
+    private readonly IAuthorizationService _authorizationService;
+    private readonly HttpListener _httpListener;
+
+    public TokenAcquirer(
+        string appRedirectUri,
+        IAuthorizationService authorizationService)
+    {
+        _authorizationService = authorizationService;
+        _httpListener = new();
+
+        if (appRedirectUri.EndsWith('/'))
+        {
+            _httpListener.Prefixes.Add(appRedirectUri);
+        }
+        else
+        {
+            _httpListener.Prefixes.Add(appRedirectUri + '/');
+        }
+    }
+
+    public async Task<string> GetToken(string[] scopes, CancellationToken cancellationToken)
+    {
+        var url = await _authorizationService.GetUserAuthorizeUrl(scopes, cancellationToken);
+
+        var processInfo = new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        };
+        Process.Start(processInfo);
+
+        _httpListener.Start();
+        var context = await _httpListener.GetContextAsync();
+        var query = context.Request.QueryString;
+
+        var code = query["code"]!;
+        var state = query["state"]!;
+        var token = await _authorizationService.GetNewAccessToken(code, state, cancellationToken);
+
+        _httpListener.Stop();
+        return token;
+    }
+
+    public Task<string> GetExistingToken(CancellationToken cancellationToken)
+    {
+        return _authorizationService.GetExistingAccessToken(cancellationToken);
+    }
+}
