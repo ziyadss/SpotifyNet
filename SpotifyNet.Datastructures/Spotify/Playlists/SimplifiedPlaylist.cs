5cd476cd09260a7375b53f575ceec88caad9635b
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-04-03 16:07:56 +0200

    Fix playlist track data structure

--- SpotifyNet.Datastructures/Spotify/Playlists/SimplifiedPlaylist.cs
+++ SpotifyNet.Datastructures/Spotify/Playlists/SimplifiedPlaylist.cs
@@ -1,61 +1,55 @@
-﻿using System.Text.Json.Serialization;
+﻿using SpotifyNet.Datastructures.Spotify.Users;
+using System.Text.Json.Serialization;
 
 namespace SpotifyNet.Datastructures.Spotify.Playlists;
 
 public class SimplifiedPlaylist
 {
     [JsonPropertyName("collaborative")]
-    public required bool Collaborative { get; init; }
+    public bool? Collaborative { get; init; }
 
     [JsonPropertyName("description")]
-    public required string? Description { get; init; }
+    public string? Description { get; init; }
+
+    [JsonPropertyName("external_urls")]
+    public ExternalUrls? ExternalUrls { get; init; }
 
     [JsonPropertyName("href")]
-    public required string Href { get; init; }
+    public string? Href { get; init; }
 
     [JsonPropertyName("id")]
-    public required string Id { get; init; }
+    public string? Id { get; init; }
 
     [JsonPropertyName("images")]
-    public required Image[] Images { get; init; }
+    public Image[]? Images { get; init; }
 
     [JsonPropertyName("name")]
-    public required string Name { get; init; }
+    public string? Name { get; init; }
 
     [JsonPropertyName("owner")]
-    public required SimplifiedPlaylistUserObject Owner { get; init; }
+    public User? Owner { get; init; }
 
     [JsonPropertyName("public")]
-    public required bool? Public { get; init; }
+    public bool? Public { get; init; }
 
     [JsonPropertyName("snapshot_id")]
-    public required string SnapshotId { get; init; }
+    public string? SnapshotId { get; init; }
 
     [JsonPropertyName("tracks")]
-    public required Followers Tracks { get; init; }
+    public SimplifiedPlaylistTracks? Tracks { get; init; }
 
     [JsonPropertyName("type")]
-    public required string Type { get; init; }
+    public ItemType? Type { get; init; }
 
     [JsonPropertyName("uri")]
-    public required string Uri { get; init; }
-
+    public string? Uri { get; init; }
 }
 
-public class SimplifiedPlaylistUserObject
+public class SimplifiedPlaylistTracks
 {
     [JsonPropertyName("href")]
-    public required string Href { get; init; }
+    public string? Href { get; init; }
 
-    [JsonPropertyName("id")]
-    public required string Id { get; init; }
-
-    [JsonPropertyName("type")]
-    public required string Type { get; init; }
-
-    [JsonPropertyName("uri")]
-    public required string Uri { get; init; }
-
-    [JsonPropertyName("display_name")]
-    public required string? DisplayName { get; init; }
+    [JsonPropertyName("total")]
+    public int? Total { get; init; }
 }
