969d8266afcd8185991ffeffdbc5edb39f4897ed
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-04-03 18:14:20 +0200

    Create AudioFeaturesSet.cs

---
+++ SpotifyNet.Datastructures/Spotify/Tracks/AudioFeaturesSet.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpotifyNet.Datastructures.Spotify.Tracks;
+
+public class AudioFeaturesSet
+{
+    [JsonPropertyName("audio_features")]
+    public required AudioFeatures[] AudioFeatures { get; init; }
+}
