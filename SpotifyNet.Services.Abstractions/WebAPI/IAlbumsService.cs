07477ad4382b473868134b360dd9ac66c2ac7547
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-02 22:52:36 +0300

    Complete AlbumsService

--- SpotifyNet.Services.Abstractions/WebAPI/IAlbumsService.cs
+++ SpotifyNet.Services.Abstractions/WebAPI/IAlbumsService.cs
@@ -1,4 +1,6 @@
 ﻿using SpotifyNet.Datastructures.Spotify.Albums;
+using SpotifyNet.Datastructures.Spotify.Tracks;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,5 +10,31 @@ public interface IAlbumsService
 {
     Task<Album> GetAlbum(
         string albumId,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<Album>> GetAlbums(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<SimplifiedTrack>> GetTracks(
+        string albumId,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<SavedAlbum>> GetUserSavedAlbums(
+        CancellationToken cancellationToken = default);
+
+    Task SaveAlbums(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken = default);
+
+    Task UnsaveAlbums(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<bool>> AreAlbumsSaved(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<SimplifiedAlbum>> GetNewReleases(
         CancellationToken cancellationToken = default);
 }
--- SpotifyNet.Services/WebAPI/AlbumsService.cs
+++ SpotifyNet.Services/WebAPI/AlbumsService.cs
@@ -1,8 +1,11 @@
 ﻿using SpotifyNet.Datastructures.Spotify.Albums;
+using SpotifyNet.Datastructures.Spotify.Authorization;
+using SpotifyNet.Datastructures.Spotify.Tracks;
 using SpotifyNet.Repositories.Interfaces;
 using SpotifyNet.Services.Interfaces;
 using SpotifyNet.Services.Interfaces.WebAPI;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -32,5 +35,91 @@ public class AlbumsService : IAlbumsService
         var album = await _webAPIRepository.GetAlbum(albumId, accessToken, cancellationToken);
 
         return album;
+    }
+
+    public async Task<IEnumerable<Album>> GetAlbums(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken)
+    {
+        var requiredScopes = Array.Empty<string>();
+
+        var accessToken = await _authorizationService.GetAccessToken(requiredScopes, cancellationToken);
+
+        var albums = await _webAPIRepository.GetAlbums(albumIds, accessToken, cancellationToken);
+
+        return albums;
+    }
+
+
+    public async Task<IEnumerable<SimplifiedTrack>> GetTracks(
+        string albumId,
+        CancellationToken cancellationToken)
+    {
+        var requiredScopes = Array.Empty<string>();
+
+        var accessToken = await _authorizationService.GetAccessToken(requiredScopes, cancellationToken);
+
+        var tracks = await _webAPIRepository.GetAlbumTracks(albumId, accessToken, cancellationToken);
+
+        return tracks;
+    }
+
+    public async Task<IEnumerable<SavedAlbum>> GetUserSavedAlbums(
+        CancellationToken cancellationToken)
+    {
+        var requiredScopes = new[] { AuthorizationScope.UserLibraryRead };
+
+        var accessToken = await _authorizationService.GetAccessToken(requiredScopes, cancellationToken);
+
+        var albums = await _webAPIRepository.GetSavedAlbums(accessToken, cancellationToken);
+
+        return albums;
+    }
+
+    public async Task SaveAlbums(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken)
+    {
+        var requiredScopes = new[] { AuthorizationScope.UserLibraryModify };
+
+        var accessToken = await _authorizationService.GetAccessToken(requiredScopes, cancellationToken);
+
+        await _webAPIRepository.SaveAlbums(albumIds, accessToken, cancellationToken);
+    }
+
+    public async Task UnsaveAlbums(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken)
+    {
+        var requiredScopes = new[] { AuthorizationScope.UserLibraryModify };
+
+        var accessToken = await _authorizationService.GetAccessToken(requiredScopes, cancellationToken);
+
+        await _webAPIRepository.SaveAlbums(albumIds, accessToken, cancellationToken);
+    }
+
+    public async Task<IEnumerable<bool>> AreAlbumsSaved(
+        IEnumerable<string> albumIds,
+        CancellationToken cancellationToken)
+    {
+        var requiredScopes = new[] { AuthorizationScope.UserLibraryRead };
+
+        var accessToken = await _authorizationService.GetAccessToken(requiredScopes, cancellationToken);
+
+        var result = await _webAPIRepository.AreAlbumsSaved(albumIds, accessToken, cancellationToken);
+
+        return result;
+    }
+
+    public async Task<IEnumerable<SimplifiedAlbum>> GetNewReleases(
+        CancellationToken cancellationToken)
+    {
+        var requiredScopes = Array.Empty<string>();
+
+        var accessToken = await _authorizationService.GetAccessToken(requiredScopes, cancellationToken);
+
+        var releases = await _webAPIRepository.GetNewReleases(accessToken, cancellationToken);
+
+        return releases;
     }
 }
